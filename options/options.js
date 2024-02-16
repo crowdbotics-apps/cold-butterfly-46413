6eb07094a50122bd29564d78e0b52ff66dd95bea
Crowdbotics <team@crowdbotics.com>
2024-02-16 18:24:37 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "cold-butterfly-46413",
+  url: "https://cold-butterfly-46413.botics.co",
+  api: "https://cold-butterfly-46413.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
